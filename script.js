b835f85b904982a7be03a3eb67edac3a2b5433b3
Dorian Patterson <imaginationac@gmail.com>
2023-03-17 22:06:32 -0400

    Add code for Challenge III

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // grab the button
 // Make GET request with fetch
-// Update the display div with the response
+// Update the header element with the name
+// Update the paragraph element with the hit points (HP)
 
 let button = document.querySelector("button");
-let div = document.getElementById("displayAPIresult");
+let header = document.querySelector("header");
+let p = document.querySelector("p");
 
 let getPokeAPI = async function() {
-    let URL = "https://pokeapi.co/api/v2/";
+    let URL = "https://pokeapi.co/api/v2/pokemon/nidoking";
     let response = await fetch(URL);
     let json = await response.json();
 
-    div.innerHTML = JSON.stringify(json);
+    header.innerHTML = json.name;
+    p.innerHTML = `HP: ${json.stats[0]["base_stat"]}`;
 }
 
 button.addEventListener("click", getPokeAPI);
